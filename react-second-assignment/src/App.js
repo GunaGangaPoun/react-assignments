06556a360f0189bb9c9ceb36903b538e0fd7c248
GunaGangaPoun <64207940+GunaGangaPoun@users.noreply.github.com>
2020-05-21 22:49:16 +0530

    initial project setup

--- react-second-assignment/src/App.js
+++ react-second-assignment/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
 import './App.css';
 
 class App extends Component {

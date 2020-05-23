32d12e274df97ff267ad5041b67c196dce703545
GunaGangaPoun <64207940+GunaGangaPoun@users.noreply.github.com>
2020-05-23 13:16:52 +0530

    Input Field with listener
    Create an input field with a change listener which outputs the length of the entered text below (eg: in a paragraph)

--- react-second-assignment/src/App.js
+++ react-second-assignment/src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    userInput:''
+  }
+  changedInputHandler = (event) =>{
+    const userInput = event.target.value;
+    this.setState({userInput:userInput});
+  }
   render() {
     return (
       <div className="App">
-        <p>Hello Everyone!</p>
+        <p>UserInput</p>
+        <input type="text" 
+            onChange={this.changedInputHandler.bind(this)} 
+            value={this.state.userInput}
+            />
+        <p>{this.state.userInput.length}</p>
       </div>
     );
   }

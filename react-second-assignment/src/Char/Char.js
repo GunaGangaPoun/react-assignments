4aa8ba57737998723e847f41c079512723556d05
GunaGangaPoun <64207940+GunaGangaPoun@users.noreply.github.com>
2020-05-23 13:50:12 +0530

    Create new component (=>Char  component)
    Create another component(=>Char component) and style it as inlinebox (   display:"inline-block",
            padding:"16px",
            textalign:"center",
            margin:"16px",
            border:"1px solid black")

---
+++ react-second-assignment/src/Char/Char.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const char = () => {
+    const style = {
+        display:"inline-block",
+        padding:"16px",
+        textalign:"center",
+        margin:"16px",
+        border:"1px solid black"
+    }
+    return(
+        <p style={style}>char component</p>
+        )
+}
+
+export default char;
